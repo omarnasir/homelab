53681ab20c60f041fd55a6f9b999a2fe82dfbd7c
omarnasir <momar.nasir@gmail.com>
2023-11-13 13:55:14 +0200

    unified single back script; change pihole db size

--- backup-containers.sh
+++ backup-containers.sh
@@ -2,20 +2,95 @@
 # Purpose: Backup important files to SD Card
 # For each stack listed in STACKS.txt, backup the .env, docker-compose.yml, and the
 # directory stack/config to the SD Card.
-# Use rsync to backup the data directory to the SD Card.
 
+# Declare variables
 mapfile -t STACKS < STACKS.txt
-BACKUP_DIR="/mnt/backup"
+STACKS+=("caddy")
+BACKUP_ROOT="/mnt/backup"
+
+# Declare stack helper backup functions
+backup_budget() {
+    # Actual
+    cd budget/data/actual
+    touch actual.tar.gz
+    tar --exclude=actual.tar.gz -czf actual.tar.gz .
+    mv actual.tar.gz $BACKUP_ROOT/budget/actual.tar.gz
+    cd ../../..
+
+    # ihatemoney
+    cd budget/data/ihatemoney
+    touch ihatemoney.tar.gz
+    tar --exclude=ihatemoney.tar.gz -czf ihatemoney.tar.gz .
+    mv ihatemoney.tar.gz $BACKUP_ROOT/budget/ihatemoney.tar.gz
+    cd ../../..
+
+    echo "---budget: Backed up: data"
+
+}
+
+backup_ha() {
+    cd ha
+    export $(grep -v '^#' .env | xargs)
+
+    # Influxdb
+    sudo docker exec -it influxdb influx backup --bucket homeassistant /var/lib/influxdb/backup
+    sudo docker cp influxdb:/var/lib/influxdb/backup influxdb_backup
+    sudo tar -czf $BACKUP_ROOT/ha/influxdb.tar.gz influxdb_backup
+    sudo rm -rf influxdb_backup
+    sudo docker exec -it influxdb rm -rf /var/lib/influxdb/backup
+
+    # Mariadb
+    sudo docker exec -it mariadb mariadb-dump -u $MYSQL_USER -p$MYSQL_PASSWORD $MYSQL_DATABASE > mariadb_backup.sql
+    sudo tar -czf $BACKUP_ROOT/ha/mariadb.tar.gz mariadb_backup.sql
+    sudo rm -rf mariadb_backup.sql
+
+    # unset environment vars from an .env file
+    unset $(grep -v '^#' .env | awk 'BEGIN { FS = "=" } ; { print $1 }')
+    cd ..
+
+    echo "---ha: Backed up: data"
+}
+
+# General function to backup stack files, excluding the config and data directory
+# first argument must be the stack name
+backup_stack_config() {
+    echo "Backing up: $1"
+    cd $1
+    # Create backup directory for stack
+    BACKUP_DIR=$BACKUP_ROOT/$1
+    mkdir -p $BACKUP_DIR
+    # Backup .env, docker-compose.yml and init.sh
+    files=(".env" "docker-compose.yml" "init.sh")
+    for file in "${files[@]}"; do
+        if [ -f $file ]; then
+            cp $file $BACKUP_DIR/$file
+        fi
+    done
+    # Backup config directory by tarring it
+    touch config.tar.gz
+    tar --exclude=config.tar.gz -czf config.tar.gz config/.
+    mv config.tar.gz $BACKUP_DIR/config.tar.gz
+    cd ..
+    echo "---$1: Backed up: config"
+}
+
+# Backup each stack, with stack specific backup commands
+for stack in "${STACKS[@]}"; do
+    if [ $stack == "budget" ]; then
+        backup_stack_config $stack
+        backup_budget
+    elif [ $stack == "ha" ]; then
+        backup_stack_config $stack
+        backup_ha
+    else
+        backup_stack_config $stack
+    fi
+done
 
 # Write backup date to file
 DATE=$(date +"%Y-%m-%d_%H-%M-%S")
-echo $DATE > $BACKUP_DIR/backup_date.txt
+echo $DATE > $BACKUP_ROOT/backup_date.txt
 
-for STACK in "${STACKS[@]}"
-do
-    echo "Backing up $STACK"
-    cp $STACK/.env $BACKUP_DIR/$STACK.env
-    cp $STACK/docker-compose.yml $BACKUP_DIR/$STACK.docker-compose.yml
-    tar -czf $BACKUP_DIR/$STACK.config.tar.gz $STACK/config/.
-    echo "--- Done backing up $STACK"
-done
+# Unset STACKS variable
+unset STACKS && unset DATE && unset BACKUP_ROOT && unset BACKUP_DIR
+echo "Done backing up all stacks"
