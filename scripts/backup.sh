5d7033263ea1acf5d4a60ba63f7858225a1d3b9f
omarnasir <momar.nasir@gmail.com>
2023-11-19 22:49:41 +0200

    installer/backup scripts using config.ini & parser

---
+++ scripts/backup.sh
@@ -0,0 +1,152 @@
+#! /bin/bash
+# Purpose: Backup important files
+
+# Import helper functions
+source scripts/config-parser.sh
+
+# Backup config or data directory for each stack. Run this function from the
+# directory containing the stack.
+# Args: $1: directory to backup
+backup_stack_subdir() {
+    local arg_dir=$1
+    # Check if backup target dir exists
+    [ ! -d $arg_dir ] && echo "WARNING: $arg_dir does not exist" && return 1
+    # Backup config directory by tarring it
+    touch $arg_dir.tar.gz
+    tar --exclude=$arg_dir.tar.gz -czf $arg_dir.tar.gz $arg_dir
+    mv $arg_dir.tar.gz $BACKUP_DIR/$arg_dir.tar.gz
+    echo "INFO: ---Backed up: $arg_dir"
+}
+
+restore_stack_subdir() {
+    local arg_dir=$1
+    # Check if target tar of the backup dir exists
+    [ ! -f $arg_dir.tar.gz ] && echo "WARNING: $arg_dir.tar.gz does not exist" && return 1
+    # Restore config directory by untarring it
+    tar -xzf $arg_dir.tar.gz
+    rm $arg_dir.tar.gz
+    echo "INFO: ---Restored: $arg_dir"
+}
+
+get_docker_container_names() {
+    # Search docker-compose.yml for all services and return their names. Service names are found
+    # under the container_name key
+    local services=$(grep -E "container_name" docker-compose.yml | cut -d: -f2 | tr -d ' ')
+    echo $services
+}
+
+stop_docker_containers() {
+    # Stop all docker containers for the current stack
+    local services=$(get_docker_container_names)
+    for service in $services; do
+        docker stop $service
+    done
+}
+
+start_docker_containers() {
+    # Start all docker containers for the current stack
+    local services=$(get_docker_container_names)
+    for service in $services; do
+        docker start $service
+    done
+}
+
+# Set the stacks to be run
+arg_action=$1
+arg_stack=$2
+if [ -z "$arg_stack" ] || [ -z "$arg_action" ]; then
+    echo "ERROR: Missing required arguments.
+
+Usage: $ bash backup-containers.sh [stack]
+
+Args:
+    [action]: The action to be performed. Accepted values: backup | restore
+    [stack]: The stack to be backed up/restored. Accepted values: all | [stack_name]"
+    exit 1
+elif [ "$arg_action" != "backup" ] && [ "$arg_action" != "restore" ]; then
+	echo "ERROR: Invalid action"
+	exit 1
+fi
+# Get stacks to backup
+parse_config_file $arg_stack
+process_stacks $arg_stack
+
+# Declare variables
+# Write backup date to file
+ROOT="$HOME/backups"
+
+backup() {
+    BACKUP_ROOT="$ROOT/$(date +"%Y-%m-%d")"
+    [ ! -d $ROOT ] && mkdir -p $ROOT
+    [ ! -d $BACKUP_ROOT ] && mkdir -p $BACKUP_ROOT
+    echo $(date +"%Y-%m-%d_%H-%M-%S") > $BACKUP_ROOT/backup_date.txt
+
+    s=0
+    # Backup each stack, with stack specific backup commands
+    for stack in "${STACKS[@]}"; do
+        echo "INFO: Backing up: $stack"
+        if [ ! -d $stack ]; then
+            echo "WARNING: Stack $stack does not exist"
+            continue
+        fi
+        cd $stack
+        # Create backup directory for stack
+        BACKUP_DIR=$BACKUP_ROOT/$stack
+        [ ! -d $BACKUP_DIR ] && mkdir -p $BACKUP_DIR
+        # Stop all containers for the stack
+        stop_docker_containers
+        # Backup .env, docker-compose.yml and init.sh
+        files=(".env" "docker-compose.yml" "init.sh")
+        for file in "${files[@]}"; do
+            if [ -f $file ]; then
+                cp $file $BACKUP_DIR/$file
+            fi
+        done
+        BACKUP_DIRS=($(get_backup_dirs_by_stack $stack))
+        for dir in "${BACKUP_DIRS[@]}"; do
+            backup_stack_subdir $dir
+        done
+        # Start all containers for the stack
+        start_docker_containers
+        cd ..
+        s=$((s + 1))
+    done
+
+    # Copy contents of scripts directory
+    cp -r scripts $BACKUP_ROOT/
+    # Copy manager.sh, .env file and Makefile
+    cp manager.sh $BACKUP_ROOT/
+    cp .env $BACKUP_ROOT/
+    cp Makefile $BACKUP_ROOT/
+
+    (( $s > 0 )) && echo "INFO: Backed up $s stacks: ${STACKS[@]}" || \
+        echo "WARNING: No stacks backed up" 
+}
+
+restore (){
+    s=0
+    # Restore each stack.
+    for stack in "${STACKS[@]}"; do
+        echo "INFO: Restoring: $stack"
+        if [ ! -d $stack ]; then
+            echo "WARNING: Stack $stack does not exist"
+            continue
+        fi
+
+        cd $stack
+        BACKUP_DIRS=($(get_backup_dirs_by_stack $stack))
+        for dir in "${BACKUP_DIRS[@]}"; do
+            restore_stack_subdir $dir
+        done
+        cd ..
+        s=$((s + 1))
+    done
+    (( $s > 0 )) && echo "INFO: Restored $s stacks: ${STACKS[@]}" || \
+        echo "WARNING: No stacks restored"
+}
+
+if [ "$arg_action" == "backup" ]; then
+    backup
+elif [ "$arg_action" == "restore" ]; then
+    restore
+fi
---
+++ scripts/install.sh
@@ -0,0 +1,99 @@
+#! /bin/bash
+# Import helper functions
+source "scripts/config-parser.sh"
+
+# Get arguments
+arg_action=$1
+arg_stack=$2
+
+if [ -z "$arg_stack" ] || [ -z "$arg_action" ]; then
+	echo "ERROR: Missing required arguments.
+	
+Usage: $ bash install-docker-apps.sh [stack] [action]
+	
+Args:
+	[stack]: The stack to be installed. Accepted values: all | [stack_name]
+	[action]: The action to be performed. Accepted values: install | update"
+	exit 1
+elif [ "$arg_action" != "install" ] && [ "$arg_action" != "update" ]; then
+	echo "ERROR: Invalid action"
+	exit 1
+fi
+
+# Parse the stacks
+parse_config_file $arg_stack
+process_stacks $arg_stack
+
+# Set environment variables from .env file
+export $(grep -v '^#' .env | xargs)
+
+install() {
+	# ----------------- #
+	# PiHole DNS Config #
+	# Write the IP of the host to the custom.list file for pihole DNS resolution
+	echo "$HOST_IP $HOME_DOMAIN"  > pihole/config/etc-pihole/custom.list
+	# Write CNAME for pihole to resolve to the host. Define your apps here that you wish
+	# to be accessible via hostname on your local network.
+	path_to_cname="pihole/config/etc-dnsmasq.d/05-pihole-custom-cname.conf"
+	[ -z path_to_cname ] && touch $path_to_cname
+	# Add the cnames to the pihole custom-cname file
+	flag_cname_changed=0
+	for cname in "${CNAMES[@]}"
+	do
+		# Search if the cname is not in the pihole custom-cname file
+		if ! grep -q "$cname.$HOME_DOMAIN" "$path_to_cname"; then
+			echo "cname=$cname.$HOME_DOMAIN,$HOME_DOMAIN" >> "$path_to_cname"
+			flag_cname_changed=1
+		fi
+	done
+	# ----------------- #
+
+	# Add caddy to the end so it will have access to the networks of each stack
+	STACKS+=("caddy")
+
+	# Run docker-compose
+	for app in "${STACKS[@]}"
+	do
+		cd $app
+		# Run init.sh if it exists
+		[ -f init.sh ] && source init.sh
+		docker compose up -d
+		cd ..
+	done
+
+	# Restart pihole if it is running, the cname flag is set, and the
+	# pihole was not included in the stacks to be run
+	if [ -n "$(docker ps -q -f name=pihole)" ] && [ $flag_cname_changed -eq 1 ]; then
+		docker exec -it pihole pihole restartdns
+		echo "INFO: Restarting pihole DNS resolver"
+		docker restart pihole
+	fi
+
+	# Finally reload CaddyFile if it is running and the cname flag is set
+	if [ -n "$(docker ps -q -f name=caddy)" ] && [ $flag_cname_changed -eq 1 ]; then
+		echo "INFO: Reloading CaddyFile"
+		docker exec -w /etc/caddy caddy caddy reload
+	fi
+}
+
+update() {
+	# Run docker-compose
+	for app in "${STACKS[@]}"
+	do
+		cd $app
+		docker compose pull
+		docker compose up -d
+		# Remove dangling images
+		docker image prune -f
+		cd ..
+	done
+}
+
+if [ "$arg_action" == "install" ]; then
+	install
+elif [ "$arg_action" == "update" ]; then
+	update
+fi
+
+# unset environment vars from an .env file
+unset $(grep -v '^#' .env | awk 'BEGIN { FS = "=" } ; { print $1 }')
